30ec1374a9fcd2d974ae743a0c0a2eb805460835
Himanshu <hrb11182@gmail.com>
2021-06-21 23:32:46 +0530

    25.Reverse Nodes in K-Group

--- C++/Linked list/Hard/25.Reverse Nodes in K-Group.cpp
+++ C++/Linked list/Hard/25.Reverse Nodes in K-Group.cpp
@@ -34,3 +34,50 @@ The number of nodes in the list is in the range sz.
 0 <= Node.val <= 1000
 1 <= k <= sz
 */
+
+
+
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if(head == NULL || k == 1) return head;
+// If the linked list is empty or has just one element so we cant reverse it so we will return head.
+        ListNode *dummy = new ListNode(0);
+// Created a dummy node.
+        dummy->next=head;
+// Making the dummy node to point the head of the linked list.
+
+        ListNode *cur = dummy, *nex = dummy, *pre = dummy;
+// Decleration of the three dummy node.
+        int count  = 0;
+// decleration of count and initialising it to 0. 
+
+        while(cur->next != NULL){
+        	cur = cur->next;
+        	count++;
+        }
+// This loop is to find the length of the linked list.
+        while(count >= k){
+// We keep on iterating untill the count remain equal or greater then k since we are reducing the value of count by k so if count become smaller then k means we left out some element which are less then k in numbers so cant be reversed.
+        	cur = pre->next;
+// This make the cur(current) to point on the first element of the part of the list of size k.
+        	nex = cur->next;
+// This will make the nex to point on the next element of the cur (ideally second element of the sublist of size k)
+        	for(int i = 1; i<k; i++){
+// Here we need to make k - 1 iterations to reverse the linked list of size k because always there is n-1 links in the list of size n. 
+        		cur->next = nex->next;
+// Since we make cur to point on the first element (see line 62) now this line  will make cur to point on the next element of nex.
+        		nex->next = pre->next;
+// This line made the next element of nex the first element
+        		pre->next = nex;
+// This line will make the last element of the first sub list as the pre for the next sublist.
+        		nex = cur->next;
+        	}
+        	pre = cur;
+        	count-=k;
+// reducing coount by k because we reversed that number of elements of the list.
+        }
+        return dummy -> next;
+// This will return the head of the modified linked list.
+    }
+};
